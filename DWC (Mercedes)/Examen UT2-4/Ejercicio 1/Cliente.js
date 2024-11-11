985d71680c3b73b7f179024e19ac9006285a7626
Joaquin Cabezas <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2024-11-11 14:37:56 +0100

    Actualizacion

---
+++ DWC (Mercedes)/Examen UT2-4/Ejercicio 1/Cliente.js
@@ -0,0 +1,28 @@
+class Cliente {
+    constructor(nombre, apellidos, dni) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.citasPendientes = [];
+        this.historialCitas = [];
+    }
+
+    agregarCita(cita) {
+        var fechaCita = cita.fechaHora.toDateString();
+        for (let pendiente of this.citasPendientes) {
+            if (pendiente.fechaHora.toDateString() == fechaCita) {
+                console.log("Ya tienes cita.");
+            }
+        }
+        
+        var ahora = new Date();
+        if (cita.fechaHora <= ahora) {
+            console.log("No se pueden agregar citas.");
+        }
+        
+        this.citasPendientes.push(cita);
+        console.log("Cita agregada.");
+    }
+
+    
+}
